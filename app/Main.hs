9f26335911d66ffea0f11d5186d25809c5068ebf
Matt Joiner <anacrolix@gmail.com>
2021-02-26 15:21:54 +1100

    wow, optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
+
 module Main where
 
+import Options.Applicative
+import Control.Monad (join)
+
+commands = hsubparser (command "ping" (info (pure ping) idm )
+    <> command "listen" (info (pure listen) (progDesc "just listens for incoming messages"))
+    )
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = join $ execParser (info (commands <**> helper) idm)
+
+ping = putStrLn "Okay will ping!"
+listen = putStrLn "will listen"
