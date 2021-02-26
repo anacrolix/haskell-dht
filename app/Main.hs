5a810caad32bec9a747431cc06d8760b5cc1e1a9
Matt Joiner <anacrolix@gmail.com>
2021-02-26 18:45:24 +1100

    Support receiving datagrams

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,48 @@
-
 module Main where
 
-import           Control.Monad       (join)
-import           Options.Applicative
+import Control.Concurrent.Async
+import Control.Monad
+import Network.Socket
+import Network.Socket.ByteString
+import Options.Applicative
+import Text.Pretty.Simple
 
-commands = hsubparser (command "ping" (info (pure ping) idm )
-    <> command "listen" (info (pure listen) (progDesc "just listens for incoming messages"))
-    )
+commands =
+  hsubparser
+    (command "ping" (info (pure ping) idm) <>
+     command
+       "listen"
+       (info (pure Main.listen) (progDesc "just listens for incoming messages")))
 
 main :: IO ()
 main = join $ execParser (info (commands <**> helper) idm)
 
-ping = putStrLn "Okay will ping!"
-listen = putStrLn "will listen"
+createSocket =
+  withSocketsDo $ do
+    addrs <-
+      getAddrInfo
+        (Just $
+         defaultHints
+           {addrFlags = [AI_PASSIVE, AI_NUMERICHOST], addrSocketType = Datagram})
+        Nothing
+        (Just "42069")
+    pPrint addrs
+    let addr = head addrs
+    sock <- openSocket addr
+    bind sock (addrAddress addr)
+    return sock
+
+recvAndPrint sock =
+  forever $ do
+    (bs, sa) <- recvFrom sock 1500
+    putStrLn $ "read from " ++ show sa ++ ": " ++ show bs
+
+listenDoThenWait job = do
+  sock <- createSocket
+  receiver <- async $ recvAndPrint sock
+  job sock
+  wait receiver
+
+ping = listenDoThenWait print
+
+listen = listenDoThenWait $ void . pure
